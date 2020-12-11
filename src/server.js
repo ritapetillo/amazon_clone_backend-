6d2a4f25daa8da4c31b321aa79ac0a92216bf05a
Loris Cuntreri <62082748+MarelGuy@users.noreply.github.com>
2020-12-11 11:29:37 +0100

    errorhandler addition

--- src/server.js
+++ src/server.js
@@ -1,5 +1,18 @@
 const express = require('express')
 const server = express()
+const {
+    catchAll,
+    unauthorized,
+    forbidden,
+    notFound,
+} = require("./errorHandler")
 const PORT = process.env.PORT || 3001
 
-server.listen(PORT,()=>console.log('server is running on', PORT))
+// Error section (Damn that's fast)
+
+server.use(unauthorized)
+server.use(forbidden)
+server.use(notFound)
+server.use(catchAll)
+
+server.listen(PORT, () => console.log('server is running on', PORT))
